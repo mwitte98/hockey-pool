35b6f56a7cee47297d86cc75f9bf1406fed6e091
Matthew Witte <mcw9612@rit.edu>
2016-04-19 00:24:00 -0400

    Sort players displayed by id

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ActiveRecord::Base
-  has_and_belongs_to_many :players
+  has_and_belongs_to_many :players, -> { order "id ASC" }
   
   def as_json(options = {})
     super(options.merge(include: [players: {include: :team}]))
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ActiveRecord::Base
-  has_many :players
+  has_many :players, -> { order "id ASC" }
   
   def as_json(options = {})
     super(options.merge(include: :players))
