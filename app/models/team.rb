0a4db1f54797ec0a8eed7faa5d234bd4cbb03814
Matthew Witte <mcw9612@rit.edu>
2017-05-29 03:25:41 -0400

    Eliminated teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
-  has_many :players
+  has_many :players, -> { order 'last_name, first_name' }
 
   def as_json(options = {})
     super(options.merge(include: :players))
