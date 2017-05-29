c60c664cc34c96f5caa90c7a6c1f8355644a6a98
Matthew Witte <mcw9612@rit.edu>
2017-05-29 01:28:16 -0400

    Remove merged assets

--- app/assets/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-//= require angular
-//= require angular-ui-router
-//= require angular-rails-templates
-//= require angular-bootstrap
-//= require angular-animate
-//= require jquery/dist/jquery
-//= require bootstrap/dist/js/bootstrap
-//= require_tree .
