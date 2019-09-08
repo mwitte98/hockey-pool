566d102681f0f360dc15f7491ca26a3f37620243
Matthew Witte <mcw9612@rit.edu>
2019-09-07 20:52:24 -0500

    Upgrade rails from 5 to 6

--- Gemfile
+++ Gemfile
@@ -1,25 +1,29 @@
 source 'https://rubygems.org'
-ruby '2.6.2'
-gem 'rails', '5.2.3'
+ruby '2.6.4'
 
-gem 'bcrypt', '3.1.12'
+gem 'actionpack', '6.0.0'
+gem 'activemodel', '6.0.0'
+gem 'activerecord', '6.0.0'
+gem 'activesupport', '6.0.0'
+gem 'railties', '6.0.0'
+
+gem 'bcrypt', '3.1.13'
 gem 'mechanize', '2.7.6'
-gem 'newrelic_rpm', '6.2.0.354'
-gem 'nokogiri', '1.10.2'
+gem 'newrelic_rpm', '6.6.0.358'
+gem 'nokogiri', '1.10.4'
 gem 'pg', '1.1.4'
-gem 'puma', '3.12.1'
-gem 'rack-cors', '1.0.3'
-gem 'sucker_punch', '2.1.1'
+gem 'puma', '4.1.0'
+gem 'sucker_punch', '2.1.2'
 
 group :development do
-  gem 'debase', '0.2.2'
-  gem 'reek', '5.3.2'
-  gem 'rubocop', '0.67.2'
-  gem 'ruby-debug-ide', '0.6.1'
+  gem 'debase', '0.2.4'
+  gem 'reek', '5.4.0'
+  gem 'rubocop', '0.74.0'
+  gem 'ruby-debug-ide', '0.7.0'
 end
 
 group :test do
-  gem 'minitest-reporters', '1.3.6'
+  gem 'minitest-reporters', '1.4.0.beta1'
   gem 'shoulda', '3.6.0'
-  gem 'simplecov', '0.16.1'
+  gem 'simplecov', '0.17.0'
 end
--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,11 @@
 require_relative 'boot'
 
-require 'rails'
+require "rails"
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'rails/test_unit/railtie'
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,8 +14,13 @@ Bundler.require(*Rails.groups)
 module HockeyPool
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
