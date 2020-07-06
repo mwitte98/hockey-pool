9e37d0e3613eb87867dbbf7031049c014028e349
Matthew Witte <mcw9612@rit.edu>
2020-07-05 23:22:47 -0500

    Display and validate positions when creating entries

--- client/src/app/create-entry/create-entry.component.ts
+++ client/src/app/create-entry/create-entry.component.ts
@@ -1,7 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 import { EntriesService } from '../shared/services/entries.service';
 import { TeamsService } from '../shared/services/teams.service';
@@ -17,6 +18,12 @@ export class CreateEntryComponent implements OnInit {
   teams: Team[];
   errors: string[] = [];
   entryForm: FormGroup;
+  numbersOfPositions = {
+    Center: 0,
+    Winger: 0,
+    Defenseman: 0,
+    Goalie: 0
+  };
 
   constructor(
     private router: Router,
@@ -34,17 +41,41 @@ export class CreateEntryComponent implements OnInit {
         } else {
           this.teamsService.get().subscribe((teams: Team[]) => {
             this.teams = teams;
-            this.entryForm = this.fb.group({
-              name: ['', Validators.required]
-            });
+            this.entryForm = this.fb.group(
+              { name: ['', Validators.required] },
+              {
+                validators: [
+                  this.positionsValidator('Center', 4, 5),
+                  this.positionsValidator('Winger', 4, 5),
+                  this.positionsValidator('Defenseman', 5, 6),
+                  this.positionsValidator('Goalie', 2, 2)
+                ]
+              }
+            );
             this.teams.map((team: Team) => {
               this.entryForm.addControl(team.name, new FormControl('', Validators.required));
             });
+            this.entryForm.valueChanges
+              .pipe(distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b)))
+              .subscribe(() => {
+                this.setNumbersOfPositions();
+                this.entryForm.updateValueAndValidity();
+              });
           });
         }
       }
     });
   }
+
+  positionsValidator = (position: string, min: number, max: number): ValidatorFn => {
+    return (): ValidationErrors | null => {
+      const numberOfPosition = this.numbersOfPositions[position];
+      if (numberOfPosition < min || numberOfPosition > max) {
+        return { [position]: true };
+      }
+      return null;
+    };
+  };
 
   trackByTeamId(_index: number, team: Team): number {
     return team.id;
@@ -52,6 +83,26 @@ export class CreateEntryComponent implements OnInit {
 
   trackByPlayerId(_index: number, player: Player): number {
     return player.id;
+  }
+
+  setNumbersOfPositions(): void {
+    const numbersOfPositions = {
+      Center: 0,
+      Winger: 0,
+      Defenseman: 0,
+      Goalie: 0
+    };
+    const formData = this.entryForm.getRawValue();
+    for (const formField of Object.keys(formData)) {
+      if (formField !== 'name') {
+        const team = this.teams.find((t) => t.name === formField);
+        const player = team.players.find((p) => p.id === formData[formField]);
+        if (player) {
+          numbersOfPositions[player.position]++;
+        }
+      }
+    }
+    this.numbersOfPositions = numbersOfPositions;
   }
 
   submitForm(): void {
