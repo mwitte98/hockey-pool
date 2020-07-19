721662546d284ba2bbb09944549fbda26767459f
Matthew Witte <mcw9612@rit.edu>
2020-07-19 18:29:34 -0500

    Add see rules dialog to create entry page

---
+++ client/src/app/create-entry/see-rules-dialog.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './see-rules-dialog.component.html'
+})
+export class SeeRulesDialogComponent {}
--- client/src/app/material.module.ts
+++ client/src/app/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -18,6 +19,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatButtonModule,
     MatCardModule,
     MatCheckboxModule,
+    MatDialogModule,
     MatExpansionModule,
     MatIconModule,
     MatInputModule,
@@ -33,6 +35,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatButtonModule,
     MatCardModule,
     MatCheckboxModule,
+    MatDialogModule,
     MatExpansionModule,
     MatIconModule,
     MatInputModule,
