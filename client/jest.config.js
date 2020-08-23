c6fc09e4a2667faf7460e63f70dac5d5a9b404be
Matthew Witte <mcw9612@rit.edu>
2020-08-23 14:13:33 -0500

    Remove HTML code coverage (#61)

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'jest-preset-angular',
   roots: ['src'],
-  setupFilesAfterEnv: ['./src/setup-jest.ts']
+  setupFilesAfterEnv: ['./src/setup-jest.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', 'html$']
 };
