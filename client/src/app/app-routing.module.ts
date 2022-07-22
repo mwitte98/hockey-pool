e9e0779958e101a200e3db2b971c3f192e517a8b
Matthew Witte <mcw9612@rit.edu>
2022-07-21 23:45:22 -0500

    Upgrade to Angular 14 (#96)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,33 +9,17 @@ import { CreateEntryComponent } from './create-entry/create-entry.component';
 import { HomeComponent } from './home/home.component';
 import { PlayerStatsComponent } from './player-stats/player-stats.component';
 
+const titlePrefix = 'NHL Playoff Pool - ';
+
 const appRoutes: Routes = [
-  {
-    path: 'admin/entries',
-    component: AdminEntriesComponent
-  },
-  {
-    path: 'admin/teams',
-    component: AdminTeamsComponent
-  },
-  {
-    path: 'admin/settings',
-    component: AdminSettingsComponent
-  },
-  {
-    path: 'entry/new',
-    component: CreateEntryComponent
-  },
-  {
-    path: 'login',
-    component: AuthComponent
-  },
-  {
-    path: 'register',
-    component: AuthComponent
-  },
-  { path: 'player-stats', component: PlayerStatsComponent },
-  { path: '', component: HomeComponent },
+  { path: 'admin/entries', component: AdminEntriesComponent, title: `${titlePrefix}Admin - Entries` },
+  { path: 'admin/teams', component: AdminTeamsComponent, title: `${titlePrefix}Admin - Teams` },
+  { path: 'admin/settings', component: AdminSettingsComponent, title: `${titlePrefix}Admin - Settings` },
+  { path: 'entry/new', component: CreateEntryComponent, title: `${titlePrefix}Admin - Create Entry` },
+  { path: 'login', component: AuthComponent, title: `${titlePrefix}Login` },
+  { path: 'register', component: AuthComponent, title: `${titlePrefix}Register` },
+  { path: 'player-stats', component: PlayerStatsComponent, title: `${titlePrefix}Player Stats` },
+  { path: '', component: HomeComponent, title: `${titlePrefix}Home` },
   { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
