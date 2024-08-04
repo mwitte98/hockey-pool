843ec19afdb551f6edf8688de06b6c076f7d99ce
Matthew Witte <mcw9612@rit.edu>
2024-08-04 15:08:50 -0500

    Bump backend versions (#139)

--- lib/id_helper.rb
+++ lib/id_helper.rb
@@ -1,7 +1,7 @@
 module IdHelper
   class << IdHelper
     def to_s(object)
-      object['id'] = object['_id']['$oid'] if object['_id']
+      object['id'] = object['_id'] if object['_id']
       object.delete('_id')
     end
   end
