0338f0920a7faabf3463d0bb3b883a070001b913
Matthew Witte <mcw9612@rit.edu>
2024-09-07 02:23:11 -0500

    Update to Angular 18's new output path (#143)

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
   def base
-    send_data File.open("#{Rails.root}/client/dist/index.html"), type: 'text/html; charset=utf-8', disposition: 'inline'
+    send_data File.open("#{Rails.root}/client/dist/browser/index.html"), type: 'text/html; charset=utf-8',
+                                                                         disposition: 'inline'
   end
 end
