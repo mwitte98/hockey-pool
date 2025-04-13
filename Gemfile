efd5ef988d9acfcb02b637a6e4609aae004eddd5
Matthew Witte <mcw9612@rit.edu>
2025-04-12 19:07:43 -0500

    Upgrade to Ruby 3.4, Rails 8, and Node 22 (#147)
    * Upgrade to Ruby 3.4, Rails 8, and Node 22
    
    * Update engines in package-lock.json

--- Gemfile
+++ Gemfile
@@ -1,20 +1,20 @@
 source 'https://rubygems.org'
-ruby '3.3.4'
+ruby '3.4.2'
 
-gem 'actionpack', '7.1.3.4'
-gem 'activemodel', '7.1.3.4'
-gem 'activesupport', '7.1.3.4'
-gem 'railties', '7.1.3.4'
+gem 'actionpack', '8.0.2'
+gem 'activemodel', '8.0.2'
+gem 'activesupport', '8.0.2'
+gem 'railties', '8.0.2'
 
 gem 'bcrypt', '3.1.20'
-gem 'mechanize', '2.12.0'
-gem 'mongoid', '9.0.1'
-gem 'nokogiri', '1.16.7'
-gem 'puma', '6.4.2'
+gem 'mechanize', '2.14.0'
+gem 'mongoid', '9.0.6'
+gem 'nokogiri', '1.18.7'
+gem 'puma', '6.6.0'
 
 group :development do
-  gem 'debug', '1.9.2'
-  gem 'rubocop', '1.65.1'
+  gem 'debug', '1.10.0'
+  gem 'rubocop', '1.75.2'
 end
 
 group :test do
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,26 +8,24 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.cache_classes = true
+  # While tests run files are not watched, reloading is not necessary.
+  config.enable_reloading = false
 
-  # Eager loading loads your whole application. When running a single test locally,
-  # this probably isn't necessary. It's a good idea to do in a continuous integration
-  # system, or in some way before deploying your code.
+  # Eager loading loads your entire application. When running a single test locally,
+  # this is usually not necessary, and can slow down your test suite. However, it's
+  # recommended that you enable it in continuous integration systems to ensure eager
+  # loading is working properly before deploying your code.
   config.eager_load = ENV['CI'].present?
 
-  # Configure public file server for tests with Cache-Control for performance.
-  config.public_file_server.enabled = true
-  config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
-  }
+  # Configure public file server for tests with cache-control for performance.
+  config.public_file_server.headers = { 'cache-control' => "public, max-age=#{1.hour.to_i}" }
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  # Show full error reports.
+  config.consider_all_requests_local = true
   config.cache_store = :null_store
 
-  # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+  # Render exception templates for rescuable exceptions and raise for other exceptions.
+  config.action_dispatch.show_exceptions = :rescuable
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
@@ -35,9 +33,6 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
-  # Raise exceptions for disallowed deprecations.
-  config.active_support.disallowed_deprecation = :raise
-
-  # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
+  # Raise error when a before_action's only/except options reference missing actions.
+  config.action_controller.raise_on_missing_callback_actions = true
 end
